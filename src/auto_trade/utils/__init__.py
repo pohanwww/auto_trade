7c6a77bdc110ce64f38dfcc41269a4e2a51f4949
pohanwww <pohanwww@gmail.com>
2025-11-24 09:53:33 +0800

    Fix: Export get_timeframe_delta from utils module

--- src/auto_trade/utils/__init__.py
+++ src/auto_trade/utils/__init__.py
@@ -1,5 +1,9 @@
 """工具模組"""
 
-from .time_utils import calculate_and_wait_to_next_execution, wait_seconds
+from .time_utils import (
+    calculate_and_wait_to_next_execution,
+    get_timeframe_delta,
+    wait_seconds,
+)
 
-__all__ = ["calculate_and_wait_to_next_execution", "wait_seconds"]
+__all__ = ["calculate_and_wait_to_next_execution", "get_timeframe_delta", "wait_seconds"]
